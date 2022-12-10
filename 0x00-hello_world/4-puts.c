dfe86e2795385787f007b8730fbad68d58a3cdab
chuksugo <rhemekas.inc@gmail.com>
2022-12-10 02:53:52 -0800

    4. Hello, puts redo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");/*multilingual puzzle*/
 	return (0);
 }
